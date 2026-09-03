5889321d793efd5278164b1b16c575f3a01b6a64
TW299 <some5720@ox.ac.uk>
2026-09-03 15:45:38 +0100

    Update halo.py

--- wimprates-master/wimpratesMod/halo.py
+++ wimprates-master/wimpratesMod/halo.py
@@ -283,16 +283,17 @@ class HaloModelInterpolatedt:
     """
         class which, from a given DF, samples the speed of the dark matter halo relative to Earth at a given time
         rho_dm is in Msun/kpc^3
+       Note rho_dm does not affect rate
     """
     def __init__(self,distF,rho_dm=None,v_0=None,v_esc=None,t=59.37,N=100,epsrel=1e-2):
         self.distF=distF
         self.v_0= _HALO_DEFAULTS['v_0'] * nu.km/nu.s if v_0 is None else v_0
         self.v_esc=_HALO_DEFAULTS['v_esc']*nu.km/nu.s if v_esc is None else v_esc
         self.vmax=v_max(t,v_esc,v_0)
-        Fac=3.79651645e-08
-        self.rho_dm=_HALO_DEFAULTS['rho_dm'] * nu.GeV/nu.c0**2 / nu.cm**3 if rho_dm is None else rho_dm*Fac
+        self.rho_dm=_HALO_DEFAULTS['rho_dm'] * nu.GeV/nu.c0**2 / nu.cm**3 if rho_dm is None else rho_dm
         vvec=np.linspace(0,self.vmax,N)
-        fvec=observed_speed_distfromdf(vvec,t=t,distF=distF,v_0=v_0,v_esc=v_esc,epsrel=epsrel)/rho_dm
+        fvec=observed_speed_distfromdf(vvec,t=t,distF=distF,v_0=v_0,v_esc=v_esc,epsrel=epsrel)/(rho_dm)*nu.Msolar/nu.kpc**3
+        self.interpcs=CubicSpline(vvec,fvec)
     def velocity_dist(self,v,t):
         try:
             len(v)
